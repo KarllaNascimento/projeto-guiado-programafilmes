727ccb925ce370c4b3460486f0f07b8622fc3296
Karlla Nascimento <karllas.nascimento@gmail.com>
2021-06-06 17:03:49 -0300

    Primeiras rotas

---
+++ src/controllers/tituloController.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose")
+const Titulo = require("../models/titulo")
+
+const createTitle = async(req, res) => {
+   const titulo = new Titulo({
+      _id: new mongoose.Types.ObjectId(),
+      nome: req.body.nome,
+      genero:req.body.genero,
+      descricao: req.body.descricao,
+      estudio:req.body.estudio
+   })
+
+   const filmeJaExiste = await Titulo.findOne({nome:req.body.nome})
+   if(filmeJaExiste){
+      return res.status(409).json({error: "Filme já cadastrado"})
+   }
+
+   try {
+      const newTitle = await titulo.save()
+      return res.status(201).json(newTitle)
+   } catch (err) {
+      return res.status(400).json({message: err.message})
+   }
+}
+
+const showTitle = async(req, res)=>{
+   try{
+      const titulos = await Titulo.find().populate("estudio")
+      return res.status(200).json(titulos)
+      } catch (err) {
+         res.status(500).json({message: err.message})
+      }
+}
+
+const mostraTitulosMarvel = async (req,res) =>{
+   const titulos = await Titulo.find().populate("estudio")
+   const titulosFiltrados = titulos.filter(titulo => titulo.estudio.nome == "Marvel")
+
+   return res.status(200).json(titulosFiltrados)
+}
+
+
+
+module.exports ={
+   createTitle,
+   showTitle,
+   mostraTitulosMarvel
+}
---
+++ src/routes/titulos.routes.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/tituloController")
+
+
+// criar um titulo > POST -> save()
+router.post("/", controller.createTitle)
+
+
+//Ler todos os titulos -> GET  -> find ()
+router.get("/", controller.showTitle)
+
+router.get("/marvel", controller.mostraTitulosMarvel)
+
+//Ler todos os títulos da Marvel
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
